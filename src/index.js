a5ec4b55f996575cf7f4d965d6a047ab3a5447b6
Mukul AggArwal <115641523+mukulagg606@users.noreply.github.com>
2023-04-27 15:18:01 +0530

    minor change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
